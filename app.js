bade4e7d4b8542a4eb1b6123191d358a906a39ae
Keyslime <danicasy1@gmail.com>
2020-05-04 13:55:09 +0800

    Level 4 Salting

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const md5=require("md5");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
+
 const app = express();
 
 app.use(express.static("public"));
@@ -15,9 +17,9 @@ app.use(bodyParser.urlencoded({
 
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true,
-  useUnifiedTopology:true
+  useUnifiedTopology: true
 })
-const userSchema =new mongoose.Schema( {
+const userSchema = new mongoose.Schema({
   email: String,
   password: String
 });
@@ -33,24 +35,25 @@ app.get("/login", function(req, res) {
   res.render("login");
 });
 
-
 app.post("/login", function(req, res) {
   const username = req.body.username;
-  const password = md5(req.body.password);
+  const password = req.body.password;
 
   User.findOne({
-    email: username
-  }, function(err, foundUser) {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        if (foundUser.password ===password){
-          res.render("secrets");
+      email: username
+    }, function(err, foundUser) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (foundUser) {
+          bcrypt.compare(password, foundUser.password, function(err, result) {
+            if (result === true) {
+              res.render("secrets");
+            }
+          });
         }
       }
-    }
-  });
+    });
 });
 
 app.get("/register", function(req, res) {
@@ -58,16 +61,18 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
-    email: req.body.username,
-    password: md5(req.body.password)
-  });
-  newUser.save(function(err) {
-    if (err) {
-      console.log(err);
-    } else {
-      res.render("secrets");
-    }
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
+      email: req.body.username,
+      password: hash
+    });
+    newUser.save(function(err) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("secrets");
+      }
+    });
   });
 });
 
